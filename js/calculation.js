d0e6b6e1c90753fb916f008616f0339584d668e9
Aditta Chakraborty <adittacse@gmail.com>
2023-02-18 17:20:08 +0600

    rectangle calculation added

--- js/calculation.js
+++ js/calculation.js
@@ -1,7 +1,15 @@
-//triangle calculation
+// triangle calculation
 document.getElementById("triangle-btn").addEventListener("click", function () {
     const firstValue = getValueFromInput("triangle-first");
     const secondValue = getValueFromInput("triangle-second");
     const area = 0.5 * firstValue * secondValue;
     setValueFormula("tri-a", "tri-b", firstValue, secondValue);
+});
+
+// rectangle calculation
+document.getElementById("rectangle-btn").addEventListener("click", function () {
+    const firstValue = getValueFromInput("rectangle-first");
+    const secondValue = getValueFromInput("rectangle-second");
+    const area = firstValue * secondValue;
+    setValueFormula("rec-a", "rec-b", firstValue, secondValue);
 });
